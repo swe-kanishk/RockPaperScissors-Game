4e5dd56bf4d97fbb2c4a8a38a05e23c9e50c0665
kanishk tiwari <kanishk.swe@gmail.com>
2025-01-18 04:53:10 +0530

    final commit, game done

--- script.js
+++ script.js
@@ -2,116 +2,50 @@
   const rockBtn = document.querySelector(".rock-btn");
   const paperBtn = document.querySelector(".paper-btn");
   const scissorsBtn = document.querySelector(".scissors-btn");
-
-  const userHand = document.querySelector(".user-hand");
-  const computerHand = document.querySelector(".computer-hand");
-
+  const userHandIcon = document.querySelector(".user.hand-icon");
+  const computerHandIcon = document.querySelector(".computer.hand-icon");
+  const result = document.querySelector(".result");
   const userScore = document.querySelector(".user-score");
   const computerScore = document.querySelector(".computer-score");
 
-  const result = document.querySelector(".result");
-
-  const optionsList = ["", "rock", "paper", "scissors"];
-  let userValue = null;
+  const rockIcon = "✊";
+  const paperIcon = "🤚";
+  const scissorsIcon = "✌️";
+  const iconsList = [rockIcon, paperIcon, scissorsIcon];
 
   rockBtn.addEventListener("click", () => {
-    userHand.querySelector(".hand-icon").classList.add("shakeUserHands");
-    computerHand
-      .querySelector(".hand-icon")
-      .classList.add("shakeComputerHands");
-    result.innerText = "shaking...";
-    userValue = 1;
-    play();
-  });
+    calculateResult(rockIcon, scissorsIcon)
+});
+  paperBtn.addEventListener("click", ()=>{
+    calculateResult(paperIcon, rockIcon)
+});
+  scissorsBtn.addEventListener("click", ()=>{
+    calculateResult(scissorsIcon, paperIcon)
+});
 
-  paperBtn.addEventListener("click", () => {
-    result.innerText = "shaking...";
-    userHand.querySelector(".hand-icon").classList.add("shakeUserHands");
-    computerHand
-      .querySelector(".hand-icon")
-      .classList.add("shakeComputerHands");
-
-    userValue = 2;
-    play();
-  });
-
-  scissorsBtn.addEventListener("click", () => {
-    result.innerText = "shaking...";
-    userHand.querySelector(".hand-icon").classList.add("shakeUserHands");
-    computerHand
-      .querySelector(".hand-icon")
-      .classList.add("shakeComputerHands");
-
-    userValue = 3;
-    play();
-  });
-
-  function play() {
-    const computerIndex = Math.floor(Math.random() * 3 + 1);
-    const winner = findWinner(computerIndex);
+  function calculateResult(selectedIcon, winningIcon) {
+    result.innerText = "shaking"
+    userHandIcon.innerText = "🤜";
+    computerHandIcon.innerText = "🤛";
+    userHandIcon.classList.add('shakeUserHands')
+    computerHandIcon.classList.add('shakeComputerHands')
 
     setTimeout(() => {
-      if (computerIndex === 1)
-        computerHand.querySelector(".hand-icon").innerText = "🪨";
-      else if (computerIndex === 2)
-        computerHand.querySelector(".hand-icon").innerText = "🧾";
-      else computerHand.querySelector(".hand-icon").innerText = "✂️";
+        userHandIcon.classList.remove('shakeUserHands')
+        computerHandIcon.classList.remove('shakeComputerHands')
 
-      if (userValue === 1)
-        userHand.querySelector(".hand-icon").innerText = "🪨";
-      else if (userValue === 2)
-        userHand.querySelector(".hand-icon").innerText = "🧾";
-      else userHand.querySelector(".hand-icon").innerText = "✂️";
+        const computerChoice = Math.floor(Math.random()*3)
+        computerHandIcon.innerText = iconsList[computerChoice]
+        userHandIcon.innerText = selectedIcon
 
-      userHand.querySelector(".hand-icon").classList.remove("shakeUserHands");
-      computerHand
-        .querySelector(".hand-icon")
-        .classList.remove("shakeComputerHands");
-
-      if (winner === "draw") {
-        setTimeout(
-          () => (userHand.querySelector(".hand-icon").innerText = "🤜"),
-          2000
-        );
-        setTimeout(
-          () => (computerHand.querySelector(".hand-icon").innerText = "🤛"),
-          2000
-        );
-        result.innerText = "Match Draw";
-      } else if (winner === "user") {
-        userScore.innerText = parseInt(userScore.innerText) + 1;
-        setTimeout(
-          () => (userHand.querySelector(".hand-icon").innerText = "🤜"),
-          2000
-        );
-        setTimeout(
-          () => (computerHand.querySelector(".hand-icon").innerText = "🤛"),
-          2000
-        );
-        result.innerText = "You won!";
-      } else {
-        computerScore.innerText = parseInt(computerScore.innerText) + 1;
-        setTimeout(
-          () => (userHand.querySelector(".hand-icon").innerText = "🤜"),
-          2000
-        );
-        setTimeout(
-          () => (computerHand.querySelector(".hand-icon").innerText = "🤛"),
-          2000
-        );
-        result.innerText = "computer wins";
-      }
-    }, 3000);
-  }
-  function findWinner(computerValue) {
-    if (computerValue === userValue) return "draw";
-    else {
-      if (computerValue === 1 && userValue === 2) return "computer";
-      else if (computerValue === 1 && userValue === 3) return "computer";
-      else if (computerValue === 2 && userValue === 1) return "user";
-      else if (computerValue === 2 && userValue === 3) return "user";
-      else if (computerValue === 3 && userValue === 1) return "user";
-      else if (computerValue === 3 && userValue === 2) return "computer";
-    }
+        if(userHandIcon.innerText === computerHandIcon.innerText) result.innerText = "Match Draw"
+        else if(computerHandIcon.innerText === winningIcon) {
+            result.innerText = "You won!"
+            userScore.innerText = parseInt(userScore.innerText) + 1
+        } else {
+            result.innerText = "You Loose, computer won!!"
+            computerScore.innerText = parseInt(computerScore.innerText) + 1
+        }
+    }, 1200)
   }
 })();
